b0ee58acff4849fc98303be576786b78c95d96c0
Jeetu soni <112809461+Unocoder07@users.noreply.github.com>
2025-01-18 22:01:46 +0530

    Time: 15 ms (78.06%), Space: 22 MB (53.73%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     int minCost(vector<vector<int>>& grid) {
-    int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> vis(m, vector<bool>(n));
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> dis(n, vector<bool>(m));
+        deque<pair<int, int>> dq;
         vector<vector<int>> dirs = {{0, 0}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        deque<pair<int, int>> q;
-        q.push_back({0, 0});
-        while (!q.empty()) {
-            auto p = q.front();
-            q.pop_front();
-            int i = p.first / n, j = p.first % n, d = p.second;
-            if (i == m - 1 && j == n - 1) return d;
-            if (vis[i][j]) continue;
-            vis[i][j] = true;
-            for (int k = 1; k <= 4; ++k) {
-                int x = i + dirs[k][0], y = j + dirs[k][1];
-                if (x >= 0 && x < m && y >= 0 && y < n) {
-                    if (grid[i][j] == k)
-                        q.push_front({x * n + y, d});
-                    else
-                        q.push_back({x * n + y, d + 1});
+        dq.push_back({0, 0});
+        while (!dq.empty()) {
+            auto it = dq.front();
+            dq.pop_front();
+            int r=it.first/m;
+            int c=it.first%m;
+            int cost= it.second;
+            if (r == n - 1 && c == m - 1)  return cost;
+            if (dis[r][c])continue;
+            dis[r][c] = true;
+            for (int i = 1; i <= 4; i++) {
+                int nr = r + dirs[i][0];
+                int nc = c + dirs[i][1];
+                if (nr >= 0 && nc >= 0 && nr < n && nc < m) {
+                    if (grid[r][c] == i) {
+                        dq.push_front({nr * m + nc, cost});
+                    } else {
+                        dq.push_back({nr * m + nc, cost + 1});
+                    }
                 }
             }
         }
         return -1;
-}
-
+    }
 };
